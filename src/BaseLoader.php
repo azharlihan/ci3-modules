cdf2b21dba513a92c43594289fd92b31668338da
M. Azhar Lihan Sandy <diariali0@gmail.com>
2020-12-03 13:52:13 +0700

    Add inline documentation  and ..
    Rename property and method to a more meaningful name.

--- src/BaseLoader.php
+++ src/BaseLoader.php
@@ -2,25 +2,50 @@
 namespace AzharLihan\CI3Modules;
 
 use Exception;
-use CI_Loader;
 
-class BaseLoader extends CI_Loader
+/**
+ * BaseLoader
+ * Extended CI_Loader Class
+ *
+ * @package CI3Modules
+ * @author Azhar Lihan <m@azharlihan.com>
+ */
+class BaseLoader extends \CI_Loader
 {
-	private $sectionName = null;
-	private $sections;
 
-	public function extend($name)
-	{
-		$this->view($name);
-	}
+	/**
+	 * Store an active section name
+	 *
+	 * @var string
+	 */
+	private $sectionName = '';
 
-	public function section($name)
+
+	/**
+	 * List of all sections defined in the view
+	 *
+	 * @var array
+	 */
+	private $sections = array();
+
+	/**
+	 * Open a section and start output buffering
+	 *
+	 * @param  string $sectionName
+	 * @return void
+	 */
+	public function section($sectionName)
 	{
-		if (!is_null($this->sectionName)) throw new Exception("Previous section is still open", 1);
-		$this->sectionName = $name;
+		if (!empty($this->sectionName)) throw new Exception("Previous section is still open", 1);
+		$this->sectionName = $sectionName;
 		ob_start();
 	}
 
+	/**
+	 * Close an opened section, and get it's content
+	 *
+	 * @return void
+	 */
 	public function endSection()
 	{
 		if (is_null($this->sectionName)) throw new Exception("No section opened", 2);
@@ -28,10 +53,29 @@ class BaseLoader extends CI_Loader
 		$this->sectionName = null;
 	}
 
-	public function renderSection($name)
+	/**
+	 * renderSection
+	 *
+	 * @param  string $sectionName
+	 * @return void
+	 */
+	public function renderSection($sectionName)
 	{
-		if (isset($this->sections[$name])) {
-			echo $this->sections[$name];
+		if (isset($this->sections[$sectionName])) {
+			echo $this->sections[$sectionName];
 		}
+	}
+
+	/**
+	 * Define a layout and call CI_Loader view method based a layout name
+	 * This function must be called at the end of the view file
+	 *
+	 * @param  string $layoutName
+	 * @return void
+	 */
+	public function extend($layoutName)
+	{
+		ob_clean();
+		$this->view($layoutName);
 	}
 }
