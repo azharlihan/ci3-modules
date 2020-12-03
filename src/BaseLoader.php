207ed28db56a49574f4d6e75f2ddde636ad37d01
M. Azhar Lihan Sandy <diariali0@gmail.com>
2020-12-03 08:56:37 +0700

    Initialize with BaseLoader Class

---
+++ src/BaseLoader.php
@@ -0,0 +1,35 @@
+<?php
+namespace AzharLihan\CI3Modules;
+
+use Exception;
+use CI_Loader;
+
+class BaseLoader extends CI_Loader
+{
+	private $sectionName = null;
+	private $sections;
+
+	public function extend($name)
+	{
+		$this->view($name);
+	}
+
+	public function section($name)
+	{
+		if (!is_null($this->sectionName)) throw new Exception("Previous section is still open", 1);
+		$this->sectionName = $name;
+		ob_start();
+	}
+
+	public function endSection()
+	{
+		if (is_null($this->sectionName)) throw new Exception("No section opened", 2);
+		$this->sections[$this->sectionName] = ob_get_clean();
+		$this->sectionName = null;
+	}
+
+	public function renderSection($name)
+	{
+		echo $this->sections[$name];
+	}
+}
